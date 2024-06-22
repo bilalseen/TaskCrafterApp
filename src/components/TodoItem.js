38460a57495e51d5d58c355c0bcb10b21458c4cf
bilalseen <bilalsen2010@gmail.com>
2024-06-22 13:48:56 +0300

    feat: Improve TodoItem component with swipeable actions
    The TodoItem component has been enhanced to include swipeable actions for marking a todo item as done and deleting it. This improvement provides a more intuitive and efficient way for users to interact with their todo items.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,99 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
-import React from "react";
+import React, { useState, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Swipeable,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { Dialog, Portal, Button } from "react-native-paper";
 
-export default function TodoItem() {
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function TodoItem({ initialIsDone = false }) {
+  const [isDone, setIsDone] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const swipeableRef = useRef(null);
+
+  const handleLeftSwipe = () => {
+    swipeableRef.current?.close();
+    setIsDone(true);
+  };
+
+  const UndoLeftSwipe = () => {
+    swipeableRef.current?.close();
+    setIsDone(false);
+  };
+
+  const handleRightSwipe = () => {
+    swipeableRef.current?.close();
+    setVisible(true);
+  };
+
+  const hideDialog = () => {
+    setVisible(false);
+  };
+
+  const confirmDelete = () => {
+    setIsDone(false);
+    setVisible(false);
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.todoText}>TodoItem</Text>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.rootContainer}>
+      <Swipeable
+        ref={swipeableRef}
+        renderLeftActions={isDone ? UndoAction : MarkAsDoneAction}
+        renderRightActions={RightSwipeActions}
+        onSwipeableLeftOpen={isDone ? UndoLeftSwipe : handleLeftSwipe}
+        onSwipeableRightOpen={handleRightSwipe}
+      >
+        <SafeAreaView style={styles.todoContainer}>
+          <Text style={[styles.todoText, isDone && styles.doneText]}>
+            TodoItem
+          </Text>
+        </SafeAreaView>
+      </Swipeable>
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>Confirm Delete</Dialog.Title>
+          <Dialog.Content>
+            <Text>Are you sure you want to delete this item?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={confirmDelete}>Delete</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </GestureHandlerRootView>
   );
 }
+const UndoAction = () => (
+  <View style={styles.undoLeftAction}>
+    <MaterialCommunityIcons name="undo" size={30} color="black" />
+  </View>
+);
+
+const MarkAsDoneAction = () => (
+  <View style={styles.leftAction}>
+    <MaterialIcons name="done" size={30} color="black" />
+  </View>
+);
+
+const RightSwipeActions = () => (
+  <View style={styles.rightAction}>
+    <MaterialIcons name="delete" size={30} color="black" />
+  </View>
+);
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  todoContainer: {
     backgroundColor: "#15101C",
     width: Dimensions.get("window").width * 0.9,
     height: Dimensions.get("window").height * 0.075,
@@ -20,8 +102,41 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 10,
   },
+  doneText: {
+    textDecorationLine: "line-through",
+    color: "#388e3c",
+  },
   todoText: {
     color: "#9E78CF",
     fontSize: 14,
+  },
+  leftAction: {
+    flex: 1,
+    backgroundColor: "#388e3c",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: 10,
+    paddingLeft: 20,
+  },
+  undoLeftAction: {
+    flex: 1,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: 10,
+    paddingLeft: 20,
+  },
+  rightAction: {
+    flex: 1,
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    borderRadius: 10,
+    paddingRight: 20,
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "600",
+    padding: 20,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Provider as PaperProvider } from "react-native-paper";
 
 import TodoItem from "../components/TodoItem";
 
@@ -8,7 +9,9 @@ export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ color: "white", fontSize: 20 }}>Home</Text>
-      <TodoItem />
+      <PaperProvider>
+        <TodoItem />
+      </PaperProvider>
     </SafeAreaView>
   );
 }
